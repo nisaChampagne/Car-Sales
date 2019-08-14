59deaeb49e3cb7dcf906f3e33b2585b00808334e
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-08-14 14:53:10 -0400

    starting

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import App from "./App";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import "bulma/css/bulma.css";
+import "./styles.scss";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+function reducer(){
+    return{
+        title: 'hello, im in redux store'
+    };
+}
+
+const store = createStore(reducer);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
